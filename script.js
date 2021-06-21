062771524d3e38fd3459adb4db7a918e8852de3c
hseiji <henrique.hst@gmail.com>
2021-06-20 23:12:41 -0400

    Trying to draw a card

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let id = document.getElementById("deckId")
 
 // Shuffle the Cards
 fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
@@ -11,6 +12,37 @@ fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
     })
     .then(data => {
         const deckId = data.deck_id
+        
         console.log(deckId);
-        console.log(data);
+        id.innerText = deckId;
+        
     })
+
+
+function test() {
+    let aux = document.getElementById("deckId").innerText
+    console.log(aux)
+}
+
+test()
+
+
+// Draw a card
+    // let str = "https://deckofcardsapi.com/api/deck/" + id.innerText + "/draw/?count=1"
+    // fetch(str)
+    //     .then(res => {
+    //         if (res.ok) {
+    //             console.log("Success")
+    //             return res.json()
+    //         } else { 
+    //             console.log("Not Successful")
+    //         }
+    //     })
+    //     .then(data => {
+    //         console.log(data);
+    //         //id.innerText = deckId
+    //     })
+
+
+
+
